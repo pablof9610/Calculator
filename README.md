33df67b515dea281e5dca541a1332e39668c8b7c
pablofesan <pablof9610@gmail.com>
2023-05-11 19:52:56 -0300

    Operating without clicking on =

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# GUI
+# Calculator GUI
 
-Objective: Create a GUI Calculator using the tkinter library in Python
+Create a GUI Calculator using the tkinter library in Python
+
+This project was created to test logical knowledgements and practice. If you can help me understanding how can i write better the code, please be free to show me how i could use logic to write better the program.
+
+Thanks!
+
+# How it works
+
+Basically, the code works with the value of the label master, which is updated as the buttons are used (numbers, operations, etc.)
+
+When i press "1" for example, this value is set to 1; so when i press "+", this value will be storaged in a variable that is know as bfnumber (before number), a global variable will storage the value that represents this operation "+" (like "1"), and the value of the label is returned to "0", waiting for a new call to change this number. When this other number is typed, the program have two numbers and a operation to calculate clicking or not in "=".
--- main.py
+++ main.py
@@ -18,87 +18,118 @@ f_op2.grid_propagate(0)
 f_buttons = tkinter.Frame(calc, width=150, height=200)
 
 # Var
+
 # used to indicate the operation the calc will do
 op = 0
+
+# used to reconize when the calc is making operations
+# without clicking on "=" and then switch the number
+# after a operation is called and concatenate the next ones
+acum = False
+
+# used to check if the operation button is called enought times
+# to show the result without clicking on the "="
+opcounter = 0
+
 # used to remind the number before the operation button is called
 bfnumber = ''
-
 
 # Functions
 
 # sets the label value when the user press a new number or call a operation
-def setlabelvalue(value, isresult=False):
+def setlabelvalue(value, isresult=False, isc = False):
     global lb_master
-    if '.' in lb_master['text'] and value == '.':
+    global acum
+    global bfnumber
+    global opcounter
+    if (('.' in lb_master['text']) and (value == '.')):
         pass
     else:
-        if isresult is True or lb_master['text'] == str(0) and value != '.':
+        if ((isresult is True) or ((lb_master['text'] == str(0)) and (value != '.')) or ((opcounter > 1) and (acum is True))):
             lb_master['text'] = str(value)
+            if (acum is True):
+                acum = False
         else:
             lb_master['text'] = lb_master['text'] + str(value)
+    if isc == True:
+        bfnumber = 0
+        opcounter = 0
 
-2
+def transform_value_type(value):
+    if '.' in value:
+        return float(value)
+    else:
+        return int(value)
+
 # calculate and update label value
-def result(value, operation):
+def result(value, operation, isequal = False):
     global lb_master
     global bfnumber
-    if '.' in value:
-        value = float(value)
-    else:
-        value = int(value)
+    value = transform_value_type(value)
     if operation == 1:
         setlabelvalue(bfnumber + value, True)
+        return bfnumber + value
     elif operation == 2:
         setlabelvalue(bfnumber - value, True)
+        return bfnumber - value
     elif operation == 3:
         setlabelvalue(bfnumber / value, True)
+        return bfnumber / value
     elif operation == 4:
         setlabelvalue(bfnumber * value, True)
+        return bfnumber * value
     else:
         unexpectederror = Tk()
         unexpectederror.title('Erro')
         unexpectederror.geometry('200x100')
         errbtn = Button(unexpectederror, text='Ok', command=unexpectederror.destroy)
-
         errbtn.pack()
+    if isequal == True:
+        opcounter = 0
 
 
 # everytime a operation is called, this function sets globally
 # the number that represents the operation in a variable
 # and same way sets globally the before number that changed
-# this is called everytime a operation is called
 def setopbfn(operation, bfn):
     global op
     global bfnumber
     global lb_master
-    lb_master['text'] = str(0)
+    global opcounter
+    global acum
+    opcounter += 1
+    if opcounter > 1:
+        if operation != op:
+            bfnumber = result(lb_master['text'], op)
+            acum = True
+        else:
+            bfnumber = result(lb_master['text'], operation)
+            acum = True
+    else: 
+        bfnumber = transform_value_type(bfn)
+        lb_master['text'] = str(0)
     op = operation
-    if '.' in bfn:
-        bfnumber = float(bfn)
-    else:
-        bfnumber = int(bfn)
 
 
 # Objetcs
-lb_master = Label(f_result, text='0',
-                  font='Arial 15')
-btn1 = Button(f_buttons, text='1', command=lambda: setlabelvalue(1), width=6, height=2)
-btn2 = Button(f_buttons, text='2', command=lambda: setlabelvalue(2), width=6, height=2)
-btn3 = Button(f_buttons, text='3', command=lambda: setlabelvalue(3), width=6, height=2)
-btn4 = Button(f_buttons, text='4', command=lambda: setlabelvalue(4), width=6, height=2)
-btn5 = Button(f_buttons, text='5', command=lambda: setlabelvalue(5), width=6, height=2)
-btn6 = Button(f_buttons, text='6', command=lambda: setlabelvalue(6), width=6, height=2)
-btn7 = Button(f_buttons, text='7', command=lambda: setlabelvalue(7), width=6, height=2)
-btn8 = Button(f_buttons, text='8', command=lambda: setlabelvalue(8), width=6, height=2)
-btn9 = Button(f_buttons, text='9', command=lambda: setlabelvalue(9), width=6, height=2)
-btn0 = Button(f_buttons, text='0', command=lambda: setlabelvalue(0), width=6, height=2)
-btn_add = Button(f_op1, text='+', width=9, height=2, command=lambda: setopbfn(1, lb_master['text']))
-btn_dim = Button(f_op1, text='-', width=9, height=2, command=lambda: setopbfn(2, lb_master['text']))
-btn_div = Button(f_op2, text='/', width=9, height=2, command=lambda: setopbfn(3, lb_master['text']))
-btn_mult = Button(f_op2, text='*', width=9, height=2, command=lambda: setopbfn(4, lb_master['text']))
-btn_pnt = Button(f_op2, text='.', width=9, height=2, command=lambda: setlabelvalue('.'))
-btn_rs = Button(f_op1, text='=', width=9, height=2, command=lambda: result(lb_master['text'], op))
-btn_clear = Button(f_op1, text='C', width=9, height=2, command=lambda: setlabelvalue(0, True))
+lb_master = Label(f_result, text='0', font='Arial 15')
+btn1 = Button(f_buttons, text='1', command=lambda: setlabelvalue(1), width=6, height=2).grid(column=0, row=1)
+btn2 = Button(f_buttons, text='2', command=lambda: setlabelvalue(2), width=6, height=2).grid(column=1, row=1)
+btn3 = Button(f_buttons, text='3', command=lambda: setlabelvalue(3), width=6, height=2).grid(column=2, row=1)
+btn4 = Button(f_buttons, text='4', command=lambda: setlabelvalue(4), width=6, height=2).grid(column=0, row=2)
+btn5 = Button(f_buttons, text='5', command=lambda: setlabelvalue(5), width=6, height=2).grid(column=1, row=2)
+btn6 = Button(f_buttons, text='6', command=lambda: setlabelvalue(6), width=6, height=2).grid(column=2, row=2)
+btn7 = Button(f_buttons, text='7', command=lambda: setlabelvalue(7), width=6, height=2).grid(column=0, row=3)
+btn8 = Button(f_buttons, text='8', command=lambda: setlabelvalue(8), width=6, height=2).grid(column=1, row=3)
+btn9 = Button(f_buttons, text='9', command=lambda: setlabelvalue(9), width=6, height=2).grid(column=2, row=3)
+btn0 = Button(f_buttons, text='0', command=lambda: setlabelvalue(0), width=6, height=2).grid(column=1, row=4)
+btn_add = Button(f_op1, text='+', width=9, height=2, command=lambda: setopbfn(1, lb_master['text'])).grid(column=0, row=1)
+btn_dim = Button(f_op1, text='-', width=9, height=2, command=lambda: setopbfn(2, lb_master['text'])).grid(column=0, row=2)
+btn_div = Button(f_op2, text='/', width=9, height=2, command=lambda: setopbfn(3, lb_master['text'])).grid(column=0, row=1)
+btn_mult = Button(f_op2, text='*', width=9, height=2, command=lambda: setopbfn(4, lb_master['text'])).grid(column=0, row=2)
+btn_pnt = Button(f_op2, text='.', width=9, height=2, command=lambda: setlabelvalue('.')).grid(column=0, row=3)
+btn_rs = Button(f_op1, text='=', width=9, height=2, command=lambda: result(lb_master['text'], op)).grid(column=0, row=3)
+btn_clear = Button(f_op1, text='C', width=9, height=2, command=lambda: setlabelvalue(0, True, True)).grid(column=0, row=4)
 
 
 # gridding & packing
@@ -107,22 +138,5 @@ f_op1.pack(side='left', anchor='nw')
 f_buttons.pack(side='left', anchor='nw')
 f_op2.pack(side='left', anchor='nw')
 lb_master.pack()
-btn1.grid(column=0, row=1)
-btn2.grid(column=1, row=1)
-btn3.grid(column=2, row=1)
-btn4.grid(column=0, row=2)
-btn5.grid(column=1, row=2)
-btn6.grid(column=2, row=2)
-btn7.grid(column=0, row=3)
-btn8.grid(column=1, row=3)
-btn9.grid(column=2, row=3)
-btn0.grid(column=1, row=4)
-btn_add.grid(column=0, row=1)
-btn_dim.grid(column=0, row=2)
-btn_rs.grid(column=0, row=3)
-btn_clear.grid(column=0, row=4)
-btn_div.grid(column=0, row=1)
-btn_mult.grid(column=0, row=2)
-btn_pnt.grid(column=0, row=3)
 
 calc.mainloop()
